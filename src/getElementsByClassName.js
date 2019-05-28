d7f719052479e93cca43a8cd880981fce56a0551
jjklee <jjlee720@gmail.com>
2019-05-28 11:04:02 -0700

    First commit

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,21 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className) {   
+  // your code here   
+  var arr = [];   
+  var helperFunc = function(node) {       
+    // check if node has a classlist and if it contains classname.       
+    // push onto arr if condition is met       
+    if (node.classList && node.classList.contains(className)) { 
+      arr.push(node);
+    }       
+    // run through child nodes and recursively call helper function for each child       
+    var lilNodes = node.childNodes;       
+    for (var i = 0; i < lilNodes.length; i++) {           
+      helperFunc(lilNodes[i]);       
+    }   
+  }   
+  helperFunc(document.body);   
+return arr; 
 };
